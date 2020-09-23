32fa903144918f5b5f560435f58d049dfae4bcb1
Tran Nguyen <69972366+trannguyen77@users.noreply.github.com>
2020-09-23 15:33:31 +0700

    Completed Product filter

--- src/components/product-filter.js
+++ src/components/product-filter.js
@@ -22,7 +22,7 @@ function ProductFilter({ products }) {
   return (
     <div className="filter-container">
       <form>
-        {/*Select type */}
+      <div className="form-field">
         <label htmlFor="type">Product type:</label>
         <select
           className="form-select"
@@ -33,8 +33,23 @@ function ProductFilter({ products }) {
         >
           {types}
         </select>
+      </div>
+      {/*Price range*/}
+      <div className="form-field">
+        <label htmlFor="price">Max price: ${price}</label>
+        <input
+          type="range"
+          name="price"
+          id="price"
+          min={minPrice}
+          max={maxPrice}
+          value={price}
+          onChange={handleChange}
+          className="range-input"
+        ></input>
+      </div>
       </form>
-    </div>
+  </div>
   );
 }
 
